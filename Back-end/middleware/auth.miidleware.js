e665eda88c191b67e5129b3ca75c8e1fb5cc3928
Hitesh Sharma <hitesh.sharma176@gmail.com>
2024-08-27 17:02:29 +0530

    downloaded bcryptjs

--- Back-end/middleware/auth.miidleware.js
+++ Back-end/middleware/auth.miidleware.js
@@ -2,10 +2,10 @@ const jwt =require('jsonwebtoken')
 const authenticateToken=(req,res,next)=>{
     const token =req.headers.authorization.split(' ')[1]
     if(!token){
-         return res.sendStatus(401); //(Token is null, unauthorized)
+         return res.sendStatus(401); 
     }else{
         jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=>{
-            if (err) return res.sendStatus(403); //Token is invalid or expired, forbidden
+            if (err) return res.sendStatus(403); 
             req.user=decoded;
             next()
         })
--- Back-end/routes/user.route.js
+++ Back-end/routes/user.route.js
@@ -1,7 +1,7 @@
 const express =require('express')
 router = express.Router();
 const User = require('../models/user.model')
-const bcrypt =require('bcrypt')
+const bcrypt =require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 router.post('/Signup',async(req,res)=>{
